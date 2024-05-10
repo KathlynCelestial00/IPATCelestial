777c501f32a2a21be78671b62babc1f97b2e65f0
KathlynCelestial00 <celestial.k.bsinfotech@gmail.com>
2024-05-10 21:59:20 +0800

    add new feature

--- dcrm/urls.py
+++ dcrm/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from . import views
 
-
 urlpatterns = [
-path('', views.home, name='home'),
-path('register', views.register, name='register')
+ path('', views.home, name='home'), 
+ path('register/', views.register, name='register'),
+ path('logout/', views.logout_user, name='logout'),
+ path('add_record/', views.add_records, name='add_record'),
+ path('update_record/<int:pk>', views.update_record, name='update_record'),
+  path('delete_record/<int:pk>', views.delete_record, name='delete_record'),
 ]
